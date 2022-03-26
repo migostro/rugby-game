1aa4913a0364b6d29d1aabdff377e345d3e1516e
migostro <migostro@hotmail.com>
2022-03-26 20:28:59 -0300

    implementação de attacker e defender

--- src/attacker.c
+++ src/attacker.c
@@ -2,28 +2,72 @@
 #include <stdio.h>
 
 // Internal headers
-#include "direction.h"
-#include "position.h"
-#include "spy.h"
-
-// Main header
-#include "attacker.h"
+#include "../include/direction.h"
+#include "../include/position.h"
+#include "../include/spy.h"
 
 // Macros
 #define UNUSED(x) (void)(x) // Auxiliary to avoid error of unused parameter
+#define LENGTH 8
+
+// Vetor ordenado pelas prioridades. Ordenando as direções em ordem horaria
+int DIRECTIONS_ATTACKER[LENGTH][2] = {DIR_UP_RIGHT,   
+                                      DIR_RIGHT,      
+                                      DIR_DOWN_RIGHT, 
+                                      DIR_DOWN,       
+                                      DIR_DOWN_LEFT,  
+                                      DIR_LEFT,       
+                                      DIR_UP_LEFT,    
+                                      DIR_UP};
 
 /*----------------------------------------------------------------------------*/
-/*                              PUBLIC FUNCTIONS                              */
+/*
+PRIVATE FUNCTIONS
+*/
 /*----------------------------------------------------------------------------*/
 
-direction_t execute_attacker_strategy(
-    position_t attacker_position, Spy defender_spy) {
-  // TODO: unused parameters, remove these lines later
-  UNUSED(attacker_position);
-  UNUSED(defender_spy);
+bool moved_atack(position_t center, direction_t direction)
+{
+    position_t new_position = move_position(center, direction);
 
-  // TODO: Implement Attacker logic here
-  return (direction_t) DIR_RIGHT;
+    // caso o defender se movimentou, retorna a direção para 'cima'
+    if (!equal_positions(center, new_position))
+    {
+        return true;
+    }
+
+    return false;
 }
+
+/*----------------------------------------------------------------------------*/
+/*
+PUBLIC FUNCTIONS
+*/
+/*----------------------------------------------------------------------------*/
+
+direction_t execute_attacker_strategy 
+        (position_t attacker_position, Spy defender_spy) {
+
+    // TODO: unused parameters, remove these lines later
+    UNUSED(defender_spy);
+
+    direction_t direction;
+
+    for (int i = 0; i < LENGTH-1; i++)
+    {
+        direction = (direction_t) {DIRECTIONS_ATTACKER[i][0], DIRECTIONS_ATTACKER[i][1]};
+
+        // Caso haja movimentação do attacker retorna a direção
+        if (moved_atack(attacker_position, direction))
+        {
+            return direction;
+        }
+    }
+
+    direction = (direction_t) {DIRECTIONS_ATTACKER[LENGTH-1][0], DIRECTIONS_ATTACKER[LENGTH-1][1]};
+
+    return direction;
+}
+
 
 /*----------------------------------------------------------------------------*/
--- src/defender.c
+++ src/defender.c
@@ -1,29 +1,100 @@
 // Standard headers
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 
 // Internal headers
-#include "direction.h"
-#include "position.h"
-#include "spy.h"
-
-// Main header
-#include "defender.h"
+#include "../include/direction.h"
+#include "../include/position.h"
+#include "../include/spy.h"
 
 // Macros
 #define UNUSED(x) (void)(x) // Auxiliary to avoid error of unused parameter
+#define LENGTH 7
+
+bool rise();
+bool moved_def(position_t center, direction_t direction);
+
+// direções tomadas pelo deffender em ordem de prioridade
+int DIRECTIONS_DEFENDER[LENGTH][2] = { DIR_DOWN,       
+                                       DIR_DOWN_LEFT,  
+                                       DIR_LEFT,       
+                                       DIR_UP_LEFT,    
+                                       DIR_UP_RIGHT,   
+                                       DIR_RIGHT,      
+                                       DIR_DOWN_RIGHT };
 
 /*----------------------------------------------------------------------------*/
-/*                              PUBLIC FUNCTIONS                              */
+/*
+PRIVATE FUNCTIONS
+*/
 /*----------------------------------------------------------------------------*/
 
-direction_t execute_defender_strategy(
-    position_t defender_position, Spy attacker_spy) {
-  // TODO: unused parameters, remove these lines later
-  UNUSED(defender_position);
-  UNUSED(attacker_spy);
+/* Sorteia se o proximo movimento será para cima
+*/
+bool rise()
+{
+    int r = rand();
 
-  // TODO: Implement Defender logic here
-  return (direction_t) DIR_LEFT;
+    if (r % 2 == 1)
+    {
+        return true;
+    }
+    
+    return false;
+}
+
+bool moved_def(position_t center, direction_t direction)
+{
+    position_t new_position = move_position(center, direction);
+
+    // caso o defender se movimentou, retorna a direção para 'cima'
+    if (!equal_positions(center, new_position))
+    {
+        return true;
+    }
+
+    return false;
+}
+
+
+/*----------------------------------------------------------------------------*/
+/*
+PUBLIC FUNCTIONS
+*/
+/*----------------------------------------------------------------------------*/
+
+direction_t execute_defender_strategy 
+        (position_t defender_position, Spy attacker_spy) {
+
+    srand(time(NULL));
+    
+    // TODO: unused parameters, remove these lines later
+    UNUSED(attacker_spy);
+
+    // sorteia para decidir se deve subir
+    if (rise() && moved_def(defender_position, (direction_t) DIR_UP))
+    {
+        return (direction_t) DIR_UP;
+    }
+
+    direction_t direction;
+
+    // caso contrario, primeiramente tenta descer, senão tenta ir para alguma outra direção
+    for (int i = 0; i < LENGTH-1; i++)
+    {
+        direction = (direction_t) {DIRECTIONS_DEFENDER[i][0], DIRECTIONS_DEFENDER[i][1]};
+
+        // Caso haja movimentação do attacker retorna a direção
+        if (moved_def(defender_position, direction))
+        {
+            return direction;
+        }
+    }
+
+    direction = (direction_t) {DIRECTIONS_DEFENDER[LENGTH-1][0], DIRECTIONS_DEFENDER[LENGTH-1][1]};
+
+    return direction;
 }
 
 /*----------------------------------------------------------------------------*/
